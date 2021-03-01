936b066d3c266642e3b2896fe951b7ed9f0b7bb1
salman nadeem <salman2198@gmail.com>
2021-03-01 19:50:18 +0100

    commit
    commit

---
+++ src/router/index.js
@@ -0,0 +1,72 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Welcome from '../views/Welcome.vue'
+import Chatroom from '../views/Chatroom.vue'
+import { projectAuth } from '../firebase/config'
+
+// auth guards
+const requireAuth = (to, from, next) => {
+  let user = projectAuth.currentUser
+  console.log('current user in auth guard: ', user)
+  if (!user) {
+    next({ name: 'Welcome' })
+  } else {
+    next()
+  }
+}
+
+const requireNoAuth = (to, from, next) => {
+  let user = projectAuth.currentUser
+  if (user) {
+    next({ name: 'Chatroom' })
+  } else {
+    next()
+  }
+}
+
+const routes = [
+  {
+    path: '/',
+    name: 'Welcome',
+    component: Welcome,
+    beforeEnter: requireNoAuth
+  },
+  {
+    path: '/chatroom',
+    name: 'Chatroom',
+    component: Chatroom,
+    beforeEnter: requireAuth
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+
+
+export default router
+
+
+function currentTime() {
+  var date = new Date(); /* creating object of Date class */
+  var hour = date.getHours();
+  var min = date.getMinutes();
+  var sec = date.getSeconds();
+  hour = updateTime(hour);
+  min = updateTime(min);
+  sec = updateTime(sec);
+  document.getElementById("clock").innerText = hour + " : " + min + " : " + sec; /* adding time to the div */
+    var t = setTimeout(function(){ currentTime() }, 1000); /* setting timer */
+}
+
+function updateTime(k) {
+  if (k < 10) {
+    return "0" + k;
+  }
+  else {
+    return k;
+  }
+}
+
+currentTime(); /* calling currentTime() function to initiate the process */
